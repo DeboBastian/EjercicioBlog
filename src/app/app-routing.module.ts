0d63af7e96e7ce2c790767b83e5b0cb5c049be79
DeboBastian <cursosdeprogramacion00@gmail.com>
2023-03-11 12:04:47 +0100

    Primer commit con rutas creadas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PostsService } from './services/posts.service';
+
+const routes: Routes = [
+  { path: '', pathMatch:'full', redirectTo: '/posts' },
+  { path: "posts", component: ListaPostsComponent},
+  { path: "blog/new", component: FormularioComponent},
+  { path: "**", redirectTo: '/posts'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
