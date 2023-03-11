308dd6445f0ff4f86050296500730f2c80635669
DeboBastian <cursosdeprogramacion00@gmail.com>
2023-03-11 19:19:12 +0100

    Lista posts completa, aunque he añadido una funcion para borrar y no funciona bien

--- src/app/components/lista-posts/lista-posts.component.ts
+++ src/app/components/lista-posts/lista-posts.component.ts
@@ -1,4 +1,7 @@
+import { PostsService } from './../../services/posts.service';
+import { Post } from './../../interfaces/post.interface';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'lista-posts',
@@ -7,4 +10,34 @@ import { Component } from '@angular/core';
 })
 export class ListaPostsComponent {
 
+  posts: Post[];
+
+  constructor(
+    private postService: PostsService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.posts = [];
+  }
+
+  
+  ngOnInit() {
+    // this.activatedRoute.params.subscribe(data => {
+       this.posts = this.postService.getAll()
+    // })
+   
+  }
+
+  onClick($event: any) {
+    if ($event.target.value === 'Todos') {
+      this.posts = this.postService.getAll()
+    } else {
+      this.posts = this.postService.getByCategory($event.target.value)
+    }
+  }
+
+
+  borrar(indice: number) {
+    this.postService.delatePost(indice)
+  }
 }
+
